d9f143a51f6e94853047aad016ef128359d2ee25
magzim <m.mowgly@gmail.com>
2015-09-28 22:43:24 +0300

    Added ehcache

--- src/main/java/com/magzim/creditcard/service/UserServiceImpl.java
+++ src/main/java/com/magzim/creditcard/service/UserServiceImpl.java
@@ -5,6 +5,8 @@ import com.magzim.creditcard.repository.UserRepository;
 import com.magzim.creditcard.util.exception.ExceptionUtil;
 import com.magzim.creditcard.util.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,10 +20,12 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository repository;
 
+    @CacheEvict(value = "users", allEntries = true)
     public User save(User user) {
         return repository.save(user);
     }
 
+    @CacheEvict(value = "users", allEntries = true)
     public void delete(int id) throws NotFoundException {
         ExceptionUtil.check(repository.delete(id), id);
     }
@@ -34,11 +38,17 @@ public class UserServiceImpl implements UserService {
         return ExceptionUtil.check(repository.getByEmail(email), "email=" + email);
     }
 
+    @Cacheable("users")
     public List<User> getAll() {
         return repository.getAll();
     }
 
+    @CacheEvict(value = "users", allEntries = true)
     public void update(User user) throws NotFoundException {
         ExceptionUtil.check(repository.save(user), user.getId());
     }
+
+    @Override
+    @CacheEvict(value = "users", allEntries = true)
+    public void cacheEvict(){}
 }
--- src/test/java/com/magzim/creditcard/service/UserServiceTest.java
+++ src/test/java/com/magzim/creditcard/service/UserServiceTest.java
@@ -43,6 +43,7 @@ public class UserServiceTest {
     @Before
     public void setUp() throws Exception {
         dbPopulator.execute();
+        service.cacheEvict();
     }
 
     @Test
